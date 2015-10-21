27cca57719530dff69446d4e52919b18e7a9425e
Tim Kellogg & Jessica Joly <student@epicodus.com>
2015-10-21 16:44:46 -0700

    restructure layouts and home pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,23 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :correct_user
+  helper_method :owns?
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Redirects away from routes where user isn't the right one
+  def correct_user
+    unless current_user && current_user == @user
+      redirect_to user_path, notice: "You don't own that!"
+    end
+  end
+
+  # Evaluates to false if the user isn't the right user
+  def owns?
+    current_user && current_user == @user
+  end
 
   protected
 
@@ -12,12 +25,7 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:sign_up) << :name
     end
 
-  private
 
 
-    def correct_user
-      unless current_user && current_user == @user
-        redirect_to root_path, notice: "You don't own that!"
-      end
-    end
+
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @photos = Photo.order(created_at: :desc)
   end
 
   def about
