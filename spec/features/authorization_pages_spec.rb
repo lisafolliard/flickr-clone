e49fe8529dffaf1f30f8276f42aa31227354f907
Tim Kellogg & Jessica Joly <student@epicodus.com>
2015-10-21 14:24:23 -0700

    Integration spec pass for user login stuff.

--- spec/features/authorization_pages_spec.rb
+++ spec/features/authorization_pages_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "authorizations" do
 
-  it "allows the user to sign into the page" do
+  it "allows the user to sign up" do
     visit root_url
     expect(page).to have_content "Login"
     expect(page).to_not have_content "Logout"
@@ -12,6 +12,19 @@ describe "authorizations" do
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
+    expect(page).to_not have_content("errors")
+    expect(page).to have_content("Logout")
+  end
+
+  it "allows the user to login" do
+    user = FactoryGirl.create(:user)
+    visit root_url
+    expect(page).to have_content "Login"
+    expect(page).to_not have_content "Logout"
+    click_link 'Login'
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
     expect(page).to_not have_content("errors")
     expect(page).to have_content("Logout")
   end
